75c823cb99093b4a8b51969738b18de8082ee111
Fortune Bekee <fortubeks@gmail.com>
2025-11-22 19:30:18 +0100

    .

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Laravel\Sanctum\HasApiTokens;
@@ -6,7 +7,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
