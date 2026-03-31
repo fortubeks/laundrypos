0c7b0b6e0358683f3b8bc48ea64f1677680dcd61
jdavyds <slimjdavids@gmail.com>
2026-03-31 17:09:54 +0100

    update role

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -87,7 +87,7 @@ class RegisteredUserController extends Controller
                 'email'          => $request->email,
                 'phone'          => $request->phone,
                 'password'       => Hash::make($request->password),
-                'role'           => 'Super Admin',
+                'role'           => 'User',
                 'otp'            => $otp,
                 'otp_expires_at' => now()->addMinutes(10),
             ]);
